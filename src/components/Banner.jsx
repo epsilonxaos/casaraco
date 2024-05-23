f5fcf2b76ab99631d3c5f9fa0fc2d12e27aa29dd
Jesus Glez <jesusgleztr94@gmail.com>
2024-05-23 17:50:10 -0600

    feat(Web): :sparkles: Banner general

---
+++ src/components/Banner.jsx
@@ -0,0 +1,7 @@
+export const Banenr = ({ src }) => {
+	return (
+		<section>
+			<img src={src} alt="Banner seccion" className="h-auto w-full max-h-[645px] object-cover object-center" />
+		</section>
+	);
+};
