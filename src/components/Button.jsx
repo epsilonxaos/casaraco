55a0a72813b658f4c0e6653313c640c0e3ca7b7c
Jesus Glez <jesusgleztr94@gmail.com>
2024-05-23 17:50:21 -0600

    feat(Web): :sparkles: Boton cafe

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -6,6 +6,14 @@ const Button = ({ children, onClick }) => {
 	);
 };
 
+const ButtonCafe = ({ children, onClick }) => {
+	return (
+		<button {...(onClick && { onClick })} type="button" className="py-2 uppercase bg-cafe w-full border-2 px-4 min-w-[80px] max-w-[300px] md:text-sm lg:text-base border-cafe text-crema hover:bg-crema hover:text-cafe transition-colors">
+			{children}
+		</button>
+	);
+};
+
 const Link = ({ children, url }) => {
 	return (
 		<a className="py-3 uppercase bg-crema border-2 px-4 min-w-[80px] md:min-w-[120px] xl:min-w-[140px] md:text-sm lg:text-base border-cafe rounded-xl text-cafe hover:bg-cafe hover:text-white transition-colors" href={url} target="_blank" rel="noopener noreferrer">
@@ -15,5 +23,6 @@ const Link = ({ children, url }) => {
 };
 
 Button.Link = Link;
+Button.Cafe = ButtonCafe;
 
 export default Button;
