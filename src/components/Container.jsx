d0445dd5c9a86315926453cb74b4a687921c71e5
Jesus Glez <jesusgleztr94@gmail.com>
2024-05-24 07:38:15 -0600

    refactor(Web): :recycle: Ahora se aceptan clases

--- src/components/Container.jsx
+++ src/components/Container.jsx
@@ -1,3 +1,5 @@
-export const Container = ({ children }) => {
-	return <div className="max-w-[1920px] mx-auto px-4">{children}</div>;
+import { twMerge } from "tailwind-merge";
+
+export const Container = ({ children, className = "" }) => {
+	return <div className={twMerge("max-w-[1920px] mx-auto px-4", className)}>{children}</div>;
 };
