1b064a35ecb594e0744696b1e55585926721c927
Jesus Glez <jesusgleztr94@gmail.com>
2024-05-23 18:11:19 -0600

    refactor(Web): :recycle: Finalizando seccion

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,6 +1,6 @@
 const Button = ({ children, onClick }) => {
 	return (
-		<button {...(onClick && { onClick })} type="button" className="py-3 uppercase bg-crema border-2 px-4 min-w-[80px] md:min-w-[120px] xl:min-w-[140px] md:text-sm lg:text-base border-cafe rounded-xl text-cafe hover:bg-cafe hover:text-white transition-colors">
+		<button {...(onClick && { onClick })} type="button" className="py-3 uppercase bg-crema border-2 px-4 min-w-[80px] md:min-w-[120px] lg:min-w-[140px] md:text-sm lg:text-base border-cafe rounded-xl text-cafe hover:bg-cafe hover:text-white transition-colors">
 			{children}
 		</button>
 	);
--- src/pages/home/Bienvenida.jsx
+++ src/pages/home/Bienvenida.jsx
@@ -12,9 +12,44 @@ export const Bienvenida = () => {
 
 				<img src="/img/logotipo.png" className="mx-auto mb-9" alt="Casa Raco" />
 
-				<Text className="max-w-[850px] mx-auto">
+				<Text className="max-w-[850px] mx-auto mb-6">
 					El inicio de <span className="font-bold">una estancia inolvidable en Parras, Coahuila.</span> Nuestro hotel boutique es más que sólo un lugar de hospedaje, se trata de un espacio cuidadosamente diseñado para que vivas una experiencia única.
 				</Text>
+
+				<ul className="flex flex-wrap lg:items-start lg:justify-center lg:gap-8 mb-8">
+					<li className="w-1/2 md:w-1/3 lg:w-auto lg:mb-0 mb-8">
+						<img src="/img/icons/sala-juntas.svg" className="size-[42px] mb-3 object-contain block mx-auto" alt="Aire acondicionado" />
+						<Text className="text-black leading-3">
+							Sala de juntas <br /> 16 personas
+						</Text>
+					</li>
+					<li className="w-1/2 md:w-1/3 lg:w-auto lg:mb-0 mb-8">
+						<img src="/img/icons/aire-ac.svg" className="size-[42px] mb-3 object-contain block mx-auto" alt="Aire acondicionado" />
+						<Text className="text-black leading-3">
+							Aire <br /> Acondicionado
+						</Text>
+					</li>
+					<li className="w-1/2 md:w-1/3 lg:w-auto lg:mb-0 mb-8">
+						<img src="/img/icons/desayuno.svg" className="size-[42px] mb-3 object-contain block mx-auto" alt="Aire acondicionado" />
+						<Text className="text-black leading-3">
+							Desayuno <br /> incluido
+						</Text>
+					</li>
+					<li className="w-1/2 md:w-1/3 lg:w-auto md:mb-0 mb-8">
+						<img src="/img/icons/maquina-cafe.svg" className="size-[42px] mb-3 object-contain block mx-auto" alt="Aire acondicionado" />
+						<Text className="text-black leading-3">
+							Maquina de Café <br /> en habitación
+						</Text>
+					</li>
+					<li className="w-1/2 md:w-1/3 lg:w-auto md:mb-0 mb-8">
+						<img src="/img/icons/zona-jardin.svg" className="size-[42px] mb-3 object-contain block mx-auto" alt="Aire acondicionado" />
+						<Text className="text-black leading-3">Zona Jardín</Text>
+					</li>
+					<li className="w-1/2 md:w-1/3 lg:w-auto md:mb-0">
+						<img src="/img/icons/comedor.svg" className="size-[42px] mb-3 object-contain block mx-auto" alt="Aire acondicionado" />
+						<Text className="text-black leading-3">Comedor</Text>
+					</li>
+				</ul>
 
 				<Button>Ver más</Button>
 			</Container>
