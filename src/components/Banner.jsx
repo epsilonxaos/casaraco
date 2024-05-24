614964808a9d49ef5ae0d5484727267f4ec41be7
Jesus Glez <jesusgleztr94@gmail.com>
2024-05-24 07:35:51 -0600

    refactor(Web): :recycle: Personalizacion de estilos

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -1,7 +1,9 @@
-export const Banenr = ({ src }) => {
+import { twMerge } from "tailwind-merge";
+
+export const Banenr = ({ src, className = "" }) => {
 	return (
 		<section>
-			<img src={src} alt="Banner seccion" className="h-auto w-full max-h-[645px] object-cover object-center" />
+			<img src={src} alt="Banner seccion" className={twMerge("h-auto w-full max-h-[645px] object-cover object-center", className)} />
 		</section>
 	);
 };
