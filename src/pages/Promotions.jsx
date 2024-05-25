0b7d18174b986e3c7943bacb444677c75f0f0071
Jesus Glez <jesusgleztr94@gmail.com>
2024-05-25 10:52:50 -0600

    refactor(Web): :recycle: Se ajustaron los componentes
    Se ajustaron los componentes para que la seccion se viera en relacion al diseño

--- src/pages/Promotions.jsx
+++ src/pages/Promotions.jsx
@@ -1,20 +1,41 @@
 import { Banner } from "../components/Banner";
+import { Container } from "../components/Container";
+import Text from "../components/Text";
 import { PromotionCard } from "./promotions/PromotionCard";
 
 export const Promotions = () => {
 	return (
 		<>
-			<Banner src={""} />
+			<Banner src={"/public/img/promociones/banner.jpg"} />
 
-			<section className="bg-zinc-100 dark:bg-zinc-900 py-12 px-4 sm:px-6 lg:px-8">
-				<div className="max-w-7xl mx-auto text-center">
-					<h2 className="text-base font-semibold tracking-wide text-brown-600 dark:text-brown-400 uppercase">Ofertas Especiales</h2>
-					<p className="mt-2 text-lg leading-6 text-zinc-600 dark:text-zinc-300">Reserva directamente con nosotros y obtén tarifas especiales para tu estancia.</p>
-				</div>
+			<section className="py-10 lg:py-16 xl:py-24">
+				<Container>
+					<div className="mx-auto text-center">
+						<Text.TitleSections className="mb-2">Ofertas Especiales</Text.TitleSections>
+						<Text className="mb-10">Reserva directamente con nosotros y obtén tarifas especiales para tu estancia.</Text>
+					</div>
+				</Container>
 
-				<PromotionCard />
-				<PromotionCard />
-				<PromotionCard />
+				<Container className="max-w-[1200px]">
+					<PromotionCard cover={"/img/promociones/1.jpg"}>
+						<Text className="mb-4">
+							Ahorra en grupos empresundefinedles <span className="font-semibold">hasta 50% de descuento</span> de sala de junta empresundefinedles
+						</Text>
+						<Text>Permítenos armar un presupuesto a tu medida para organizar tu próxima reunión de trabajo en nuestra cómoda sala de juntas con capacidad de hasta 16 personas.</Text>
+					</PromotionCard>
+					<PromotionCard cover={"/img/promociones/2.jpg"}>
+						<Text className="mb-4">
+							Recibe un <span className="font-bold">15% en reservaciones</span> de 3 noches o más
+						</Text>
+						<Text>Recibe un descuento especial al reservar directamente con nosotros.</Text>
+					</PromotionCard>
+					<PromotionCard cover={"/img/promociones/3.jpg"}>
+						<Text className="mb-4">
+							Necesitas <span className="font-bold">la casa completa da clic aquí,</span>
+						</Text>
+						<Text>Arma un paquete de estadía a la medida de tus necesidades.</Text>
+					</PromotionCard>
+				</Container>
 			</section>
 		</>
 	);
