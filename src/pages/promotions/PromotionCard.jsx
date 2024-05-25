d7ac800648503267225e3a24a1960e850a98bf68
Jesus Glez <jesusgleztr94@gmail.com>
2024-05-25 10:54:27 -0600

    refactor(Web): :recycle: optimizacion de componente

--- src/pages/promotions/PromotionCard.jsx
+++ src/pages/promotions/PromotionCard.jsx
@@ -1,17 +1,14 @@
 import Button from "../../components/Button";
 import Text from "../../components/Text";
 
-export const PromotionCard = () => {
+export const PromotionCard = ({ cover, children }) => {
 	return (
-		<article className="mt-10 flex flex-col lg:flex-row items-center justify-center lg:space-x-8">
-			<picture className="lg:w-1/2">
-				<img src="https://placehold.co/600x400" alt="Meeting room" className="rounded-lg shadow-lg" />
+		<article className="mt-12 flex flex-col lg:flex-row items-center lg:space-x-8 bg-white">
+			<picture className="w-full lg:w-1/2">
+				<img src={cover} alt="Meeting room" className="shadow-lg object-cover w-full h-[350px] md:h-[450px]" />
 			</picture>
-			<main className="mt-6 lg:mt-0 lg:w-1/2 text-center lg:text-left">
-				<Text className="mb-4">
-					Ahorra en grupos empresundefinedles <span className="font-semibold">hasta 50% de descuento</span> de sala de junta empresundefinedles
-				</Text>
-				<Text>Permítenos armar un presupuesto a tu medida para organizar tu próxima reunión de trabajo en nuestra cómoda sala de juntas con capacidad de hasta 16 personas.</Text>
+			<main className="lg:mt-0 lg:w-1/2 text-center py-10">
+				<div className="max-w-[345px] mx-auto">{children}</div>
 				<Button className="mt-6">Book Now</Button>
 			</main>
 		</article>
