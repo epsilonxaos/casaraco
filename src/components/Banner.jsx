178a1ed7c359195e229a8e6109d8c17360d4a4f3
Jesus Glez <jesusgleztr94@gmail.com>
2024-05-25 10:50:37 -0600

    fix(Web): :recycle: Correccion de nombre de Componente

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -1,6 +1,6 @@
 import { twMerge } from "tailwind-merge";
 
-export const Banenr = ({ src, className = "" }) => {
+export const Banner = ({ src, className = "" }) => {
 	return (
 		<section>
 			<img src={src} alt="Banner seccion" className={twMerge("h-auto w-full max-h-[645px] object-cover object-center", className)} />
