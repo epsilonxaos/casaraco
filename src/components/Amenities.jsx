2d82afb7200bf965cc7bdcf495dd9d1498fa3630
Jesus Glez <jesusgleztr94@gmail.com>
2024-05-25 12:38:29 -0600

    refactor(Amenidades):

---
+++ src/components/Amenities.jsx
@@ -0,0 +1,40 @@
+import Text from "./Text";
+
+export const Amenities = () => {
+	return (
+		<ul className="flex flex-wrap lg:items-start lg:justify-center lg:gap-8 mb-8">
+			<li className="w-1/2 md:w-1/3 lg:w-auto lg:mb-0 mb-8">
+				<img src="/img/icons/sala-juntas.svg" className="size-[42px] mb-3 object-contain block mx-auto" alt="Aire acondicionado" />
+				<Text className="text-black !leading-5">
+					Sala de juntas <br /> 16 personas
+				</Text>
+			</li>
+			<li className="w-1/2 md:w-1/3 lg:w-auto lg:mb-0 mb-8">
+				<img src="/img/icons/aire-ac.svg" className="size-[42px] mb-3 object-contain block mx-auto" alt="Aire acondicionado" />
+				<Text className="text-black !leading-5">
+					Aire <br /> Acondicionado
+				</Text>
+			</li>
+			<li className="w-1/2 md:w-1/3 lg:w-auto lg:mb-0 mb-8">
+				<img src="/img/icons/desayuno.svg" className="size-[42px] mb-3 object-contain block mx-auto" alt="Aire acondicionado" />
+				<Text className="text-black !leading-5">
+					Desayuno <br /> incluido
+				</Text>
+			</li>
+			<li className="w-1/2 md:w-1/3 lg:w-auto md:mb-0 mb-8">
+				<img src="/img/icons/maquina-cafe.svg" className="size-[42px] mb-3 object-contain block mx-auto" alt="Aire acondicionado" />
+				<Text className="text-black !leading-5">
+					Maquina de Café <br /> en habitación
+				</Text>
+			</li>
+			<li className="w-1/2 md:w-1/3 lg:w-auto md:mb-0 mb-8">
+				<img src="/img/icons/zona-jardin.svg" className="size-[42px] mb-3 object-contain block mx-auto" alt="Aire acondicionado" />
+				<Text className="text-black !leading-5">Zona Jardín</Text>
+			</li>
+			<li className="w-1/2 md:w-1/3 lg:w-auto md:mb-0">
+				<img src="/img/icons/comedor.svg" className="size-[42px] mb-3 object-contain block mx-auto" alt="Aire acondicionado" />
+				<Text className="text-black !leading-5">Comedor</Text>
+			</li>
+		</ul>
+	);
+};
