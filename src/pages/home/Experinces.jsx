01734d37ba220b46dec0111d9191c6395c633d89
Jesus Glez <jesusgleztr94@gmail.com>
2024-05-25 10:48:58 -0600

    feat(Web): :sparkles: Seccion de Experiencias

---
+++ src/pages/home/Experinces.jsx
@@ -0,0 +1,33 @@
+import Button from "../../components/Button";
+import { Container } from "../../components/Container";
+import Text from "../../components/Text";
+
+export const Experiences = () => {
+	return (
+		<section className="py-10 lg:py-16 xl:py-24">
+			<Container className="px-0">
+				<div className="grid grid-cols-1 md:grid-cols-3 md:items-center">
+					<div className="col-span-1 px-4 py-10 text-center lg:pr-10">
+						<div className="max-w-[350px] ml-auto">
+							<Text.TitleSections className="mb-3">Experiencias</Text.TitleSections>
+							<Text className="mb-5">Parras está lleno de magia por ser descubierta. Acompáñanos a crear una experiencia a tu medida.</Text>
+
+							<Button>Ver más</Button>
+						</div>
+					</div>
+					<div className="col-span-1 md:col-span-2 grid grid-cols-2 gap-2 lg:gap-4 md:grid-cols-3">
+						<picture className="col-span-1">
+							<img src="/public/img/home/1.png" className="h-[200px] md:h-[500px] lg:h-[650px] object-cover w-full" alt="experiencia 1" />
+						</picture>
+						<picture className="col-span-1">
+							<img src="/public/img/home/2.png" className="h-[200px] md:h-[500px] lg:h-[650px] object-cover w-full" alt="experiencia 1" />
+						</picture>
+						<picture className="col-span-2 md:col-span-1">
+							<img src="/public/img/home/3.png" className="h-[200px] md:h-[500px] lg:h-[650px] object-cover w-full" alt="experiencia 1" />
+						</picture>
+					</div>
+				</div>
+			</Container>
+		</section>
+	);
+};
